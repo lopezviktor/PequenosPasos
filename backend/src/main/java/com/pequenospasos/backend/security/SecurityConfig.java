65773b95c374c46856c05acdbedc31bcba43201b
Victor Lopez <victorlopezlopez93@gmail.com>
2026-09-08 17:32:20 +0200

    feat: enable method security to enforce @PreAuthorize annotations

--- backend/src/main/java/com/pequenospasos/backend/security/SecurityConfig.java
+++ backend/src/main/java/com/pequenospasos/backend/security/SecurityConfig.java
@@ -15,9 +15,11 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Autowired
